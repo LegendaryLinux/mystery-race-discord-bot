34681de5bf227c3a99d7588c0abba32b85b8fdb1
Legendary_Linux <chris@legendserver.info>
2017-02-16 23:37:50 -0500

    Removed SQLite database. Moved to hosted database service (MySQL).
    - Data import script is not finished yet.

--- dbInit.js
+++ dbInit.js
@@ -1,37 +1,26 @@
-let SQLite = require('sqlite3');
 let fs = require('fs');
+let mysql = require('mysql');
 
-// Delete an already existing database
-if(fs.existsSync){fs.unlink('mystery-races.sqlite', (err) => {if(err) throw err});}
+// Read and save the database connection info into an object
+fs.readFile('db-creds.json', 'utf8', (err, data) => {
+    if(err) throw err;
+    let dbInfo = JSON.parse(data);
 
-// Open the JSON file for reading
-fs.readFile('organised-formatted-games-list.json', 'utf8', (err, data) => {
-    // Hopefully nothing goes wrong
-    if(err){throw err;}
+    // Instantiate a database handle
+    let db = new mysql.createConnection({
+        host: dbInfo.host,
+        user: dbInfo.user,
+        password: dbInfo.pass,
+        database: dbInfo.dbname
+    });
 
-    // Transform the JSON into a JavaScript object
-    let randomGames = JSON.parse(data);
+    // Read and save the games list into an object
+    fs.readFile('organised-formatted-games-list.json', 'utf8', (err, data) => {
+        if(err){throw err;}
+        let randomGames = JSON.parse(data);
 
-    // Open / Create the database
-    let db = new SQLite.Database('mystery-races.sqlite');
-
-    // Create the random-games table
-    db.run("CREATE TABLE unofficial(name TEXT, genre TEXT, region TEXT, url TEXT, console TEXT, goals TEXT, pastebin TEXT)",
-        (err) => {if(err) throw err});
-
-    setTimeout(() => {
-        // Loop over the games in the unofficial JSON file
         randomGames.forEach((game) => {
-            console.log("Inserting "+game.title);
-            let insert = "INSERT INTO unofficial (name, genre, region, url, console) VALUES (?,?,?,?,?)";
-            db.run(insert, [
-                game.title,
-                JSON.stringify(game.genre),
-                JSON.stringify(game.region),
-                JSON.stringify(game.url),
-                JSON.stringify(game.console)
-            ], (err) => {
-                if(err) throw err});
+
         });
-    }, 3000);
+    });
 });
